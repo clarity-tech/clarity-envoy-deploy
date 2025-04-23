a9037af30c29195100842c512534ce455e5f84b0
mohitp <mohit@claritytech.io>
2025-04-23 18:46:45 +0530

    Update Envoy.php

--- src/Envoy.blade.php
+++ src/Envoy.blade.php
@@ -45,14 +45,23 @@ return "echo '\033[32m" .$message. "\033[0m';\n";
 
 @endsetup
 
-@success
-@slack($webhookUrl, '#deploys', ':white_check_mark: Successfully Ran Task `' . $__task . '` on '. "*$ciProjectName* $ciProjectUrl" .' environment `'. $ciEnvSlug . '` commit `'. $commitSha .'` target dir `'. $fullProjectDir . '` by '. $ciAuthor .'. *View Job* '. $ciJobUrl)
 
+@success
+if (is_null($webhookUrl)) {
+    logMessage("deployed");
+}
+else {
+    @slack($webhookUrl, '#deploys', ':white_check_mark: Successfully Ran Task `' . $__task . '` on '. "*$ciProjectName* $ciProjectUrl" .' environment `'. $ciEnvSlug . '` commit `'. $commitSha .'` target dir `'. $fullProjectDir . '` by '. $ciAuthor .'. *View Job* '. $ciJobUrl)
+}
 @endsuccess
 
 @error
-@slack($webhookUrl, '#deploys', ':exclamation: Error running Task `' . $__task . '` on '. "*$ciProjectName* $ciProjectUrl" . ' environment `'. $ciEnvSlug . '` commit `'. $commitSha .'` target dir `'. $fullProjectDir . '` by '. $ciAuthor .'. *View Job* '. $ciJobUrl)
-
+if (is_null($webhookUrl)) {
+    logMessage("error deployed");
+}
+else {
+    @slack($webhookUrl, '#deploys', ':exclamation: Error running Task `' . $__task . '` on '. "*$ciProjectName* $ciProjectUrl" . ' environment `'. $ciEnvSlug . '` commit `'. $commitSha .'` target dir `'. $fullProjectDir . '` by '. $ciAuthor .'. *View Job* '. $ciJobUrl)
+}
 @enderror
 
 
